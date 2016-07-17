f88077ea45fde6904ee46b1d2e22c561a68c6c9e
Evgeni Kappinen <evkappinen@gmail.com>
2016-07-17 18:48:16 +0300

    shutdown method for nightly calls.

--- web2local/src/main/scala/types/DataItemExtension.scala
+++ web2local/src/main/scala/types/DataItemExtension.scala
@@ -1,8 +1,5 @@
 package types
 
-import io.LocalStorage._
-import types.DataItem
-
 /**
  * Created by Evgeni Kappinen on 5/24/14.
  */
@@ -10,20 +7,22 @@ object DataItemExtension {
 
   implicit class DataItemHelper(sequence: Seq[DataItem]) {
 
-    def apply(key: String) : Seq[Any] = sequence.map((a) => a(key))
+    def apply(key: String): Seq[Any] = sequence.map((a) => a(key))
 
-    def applyTo(newKey:String)(func: (DataItem) => Any): Seq[DataItem] = sequence.map((a) => a ++ Map(newKey -> func(a)))
+    def applyTo(newKey: String)(func: (DataItem) => Any): Seq[DataItem] = sequence.map((a) => a ++ Map(newKey -> func(a)))
 
-    def slidingBy(newKey:String, window:Int)(func: (Seq[DataItem]) => Any): Seq[DataItem] = sequence.sliding(window).toList.map((a) => a.last ++ Map(newKey -> func(a)))
+    def slidingBy(newKey: String, window: Int)(func: (Seq[DataItem]) => Any): Seq[DataItem] = sequence.sliding(window).toSeq.map((a) => a.last ++ Map(newKey -> func(a)))
+
 
     def minMaxBy(key: String): Pair[Double, Double] =
       sequence.foldLeft[Pair[Double, Double]](Pair(1, 0))((a, b) => (math.min(b(key).toDouble, a._1), math.max(b(key).toDouble, a._2)))
 
+    //TODO: , size: Int = 2 and slideBy
     def meanBy(key: String): Double = sequence.toDouble(key).reduce((a, b) => b + a) / sequence.size
 
     def diffBy(key: String): Seq[DataItem] =
       (sequence.drop(1) zip sequence.dropRight(1)).
-        map((a) => a._1 ++ Map(key -> (a._1.toDouble(key) - a._2.toDouble(key))))
+        map((a) => a._1 ++ Map(key -> (a._2.toDouble(key) - a._1.toDouble(key))))
 
     def normilize(key: String): Seq[DataItem] = {
       val limits = minMaxBy(key)
@@ -39,10 +38,21 @@ object DataItemExtension {
     }
 
     def toDouble(key: String): Seq[Double] = sequence.map((a) => a.toDouble(key))
+
+    def getOrDefault[T](key: String, defaultValue: T): Seq[T] = {
+      sequence.map((a) => {
+        if (a(key) == null) {
+          defaultValue
+        } else {
+          a(key).asInstanceOf[T]
+        }
+      })
+    }
+
   }
 
   //http://rosettacode.org/wiki/Averages/Median
-  def median(sequence: Seq[Double]): Double = {
+  def median(sequence: Array[Double]): Double = {
     val sorted = sequence.sorted
     val splitted = sorted.splitAt(sorted.size / 2)
     if (sorted.size % 2 == 0) {
@@ -51,5 +61,6 @@ object DataItemExtension {
       splitted._2.head
     }
   }
+
 }
 
